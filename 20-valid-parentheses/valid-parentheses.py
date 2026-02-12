11eb102c1c1b4272cabf94733104f2291d94731c
Neha Rajvir <neha.rajvir138643@marwadiuniversity.ac.in>
2026-02-12 22:21:39 +0530

    Time: 3 ms (76.84%) | Memory: 12.5 MB (94.75%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        
+        for ch in s:
+            if ch in pairs.values():  
+                stack.append(ch)
+            else:  
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+        
+        return len(stack) == 0
