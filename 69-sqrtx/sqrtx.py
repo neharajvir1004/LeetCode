0afc78f093faeba8c5402995571ecfbe626e88d0
Neha Rajvir <neha.rajvir138643@marwadiuniversity.ac.in>
2026-03-04 23:10:55 +0530

    Time: 3 ms (54.26%) | Memory: 12.3 MB (98.99%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+        ans = 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            sq = mid * mid
+
+            if sq == x:
+                return mid
+            elif sq < x:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
